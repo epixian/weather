507ca86ae8c050babab4e2516efd0d13b341c650
epixian <epixian@epixian.com>
2019-08-15 10:58:03 -0400

    Update WeatherLocationFactory.php

--- database/factories/WeatherLocationFactory.php
+++ database/factories/WeatherLocationFactory.php
@@ -8,8 +8,9 @@ use Faker\Generator as Faker;
 $factory->define(WeatherLocation::class, function (Faker $faker) {
     return [
         // 'user_id' => factory('App\User')->create()->id,
-        'address' => $faker->postcode,
-        'lat' => $faker->randomFloat(7, -90, 90),
-        'lng' => $faker->randomFloat(7, -179.9999999, 180)
+        // 'address' => $faker->postcode,
+        'address' => '12345',
+        // 'lat' => $faker->randomFloat(7, -90, 90),
+        // 'lng' => $faker->randomFloat(7, -179.9999999, 180)
     ];
 });
