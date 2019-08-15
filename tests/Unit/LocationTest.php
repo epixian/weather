cb75762242a8323676cf81030580d7ef055b2d48
epixian <epixian@epixian.com>
2019-08-15 10:57:02 -0400

    Update LocationTest.php

--- tests/Unit/LocationTest.php
+++ tests/Unit/LocationTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit;
 
 use App\Forecast;
+use App\Location;
 use Tests\TestCase;
 use App\WeatherLocation;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -15,41 +16,33 @@ class LocationTest extends TestCase
     /** @test */
     public function locations_have_a_weather_location()
     {
-        $weatherLocation = factory('App\WeatherLocation')->create();
+        $location = factory('App\Location')->create(['postal_code' => '12345']);
 
-        $location = factory('App\Location')->make(['postal_code' => $weatherLocation->address]);
+        // creates a new location
+        $this->assertCount(1, Location::all());
 
-        $location->setWeatherLocation($weatherLocation);
+        // should also create new weather location
+        $this->assertCount(1, WeatherLocation::all());
 
-        $location->fresh();
-
-        $this->assertEquals($location->weatherLocation->id, $weatherLocation->id);
+        // and associate to the location
+        $this->assertEquals($location->weatherLocation->id, WeatherLocation::first()->id);
         $this->assertEquals($location->weatherLocation->address, $location->postal_code);
+
+        // add another location with same zip code
+        $location = factory('App\Location')->create(['postal_code' => '12345']);
+
+        // should associate existing weather location and not create new
+        $this->assertCount(2, Location::all());
+        $this->assertCount(1, WeatherLocation::all());
     }
 
     /** @test */
     public function weather_locations_have_forecasts()
     {
         $location = factory('App\WeatherLocation')->create();
-        $forecast = factory('App\Forecast')->make();
-
-        $location->addForecast($forecast);
 
         $this->assertCount(1, Forecast::all());
         $this->assertCount(1, WeatherLocation::all());
-        $this->assertEquals($location->forecasts->first()->data, $forecast->data);
-    }
-
-    /** @test */
-    public function forecasts_have_a_location()
-    {
-        $location = factory('App\WeatherLocation')->create();
-        $forecast = factory('App\Forecast')->make();
-
-        $forecast->setWeatherLocation($location);
-
-        $this->assertCount(1, Forecast::all());
-        $this->assertCount(1, WeatherLocation::all());
-        $this->assertEquals($forecast->weatherLocation, $location);
+        $this->assertEquals($location->forecasts->first()->id, Forecast::first()->id);
     }
 }
