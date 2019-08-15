c2d9d33fb52800c1fe6c4261735b1b0fe850f9bf
epixian <epixian@epixian.com>
2019-08-15 10:58:10 -0400

    Update LocationFactory.php

--- database/factories/LocationFactory.php
+++ database/factories/LocationFactory.php
@@ -7,6 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Location::class, function (Faker $faker) {
     return [
+        'name' => $faker->company,
         'address_1' => $faker->streetAddress,
         'city' => $faker->city,
         'state' => $faker->stateAbbr,
