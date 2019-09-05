073b1f112dfc68273be7416c0a3bf9965c409c65
epixian <epixian@epixian.com>
2019-09-05 17:12:18 -0400

    Update WeatherLocation.vue

--- resources/js/components/WeatherLocation.vue
+++ resources/js/components/WeatherLocation.vue
@@ -1,17 +1,23 @@
 <template>
-    <div :id="'weather-location-' + address" class="message">
+    <div :id="'weather-location-' + data.address" class="message">
         <div class="message-header">
-            <p>{{ address }} ({{ lat }}, {{ lng }})</p>
+            <p><router-link :to="href">Weather Location {{ data.address }}</router-link></p>
         </div>
-        <div class="message-body columns">
+        <div class="message-body columns is-desktop">
             <div class="column">
                 <forecast 
-                    v-for="forecast in forecasts" 
+                    v-for="forecast in data.forecasts" 
                     :key="forecast.id"
-                    :data="forecast"></forecast>
+                    :data="forecast"
+                    class="is-size-7"></forecast>
             </div>
-            <div class="column" v-if="locations">
-                <p v-for="location in locations">{{ location.address_1 }}</p>
+            <div class="column" v-if="data.locations">
+                <p 
+                    v-for="location in data.locations" 
+                    :key="location.id" 
+                    class="is-size-7">
+                    <router-link :to="'/locations/' + location.id">{{ location.address_1 }}</router-link>
+                </p>
             </div>
         </div>        
     </div>
@@ -28,13 +34,19 @@
 
         props: [ 'data' ],
 
-        data() {
-            return {
-                address: this.data.address,
-                lat: this.data.lat,
-                lng: this.data.lng,
-                forecasts: this.data.forecasts,
-                locations: this.data.locations
+        // data() {
+        //     return {
+        //         address: this.data.address,
+        //         lat: this.data.lat,
+        //         lng: this.data.lng,
+        //         forecasts: this.data.forecasts,
+        //         locations: this.data.locations
+        //     }
+        // },
+
+        computed: {
+            href: function () {
+                return '/weatherlocations/' + this.data.id;
             }
         }
     }
