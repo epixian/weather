a2e0c0b91ea99b69e53858930d1f1464044dfae0
epixian <epixian@epixian.com>
2019-09-05 17:10:58 -0400

    add weather map

--- resources/js/views/WeatherLocations.vue
+++ resources/js/views/WeatherLocations.vue
@@ -13,14 +13,22 @@
             </div>
         </div>
 
-        <div class="container section">
-            <weather-location
-                v-for="location in weatherLocations"
-                :key="location.address"
-                :data="location"></weather-location>
+        <div class="columns">
+            <div id="map" class="column is-two-thirds">
+                <iframe :src="mapUrl" style="width:100%;height:500px;border:0;"></iframe>
+            </div>
 
-            <add-weather-location @location_added="addWeatherLocation"></add-weather-location>
+            <div class="column section">
+                <weather-location
+                    v-for="location in weatherLocations"
+                    :key="location.address"
+                    :data="location"></weather-location>
+
+                <add-weather-location @location_added="addWeatherLocation"></add-weather-location>
+            </div>
         </div>
+
+        <div class="container section">Powered by <a href="https://darksky.net/poweredby/">DarkSky</a></div>
     </div>
 </template>
 
@@ -34,8 +42,51 @@
         data() {
             return {
                 weatherLocations: [],
-                message: ''
+                message: '',
+                mapEmbed: ''
             }
+        },
+
+        computed: {
+            lats() {
+                return this.weatherLocations.map(l => parseFloat(l.lat));
+            },
+            lngs() {
+                return this.weatherLocations.map(l => parseFloat(l.lng));
+            },
+            minLat() {
+                return Math.min(...this.lats);
+            },
+            maxLat() {
+                return Math.max(...this.lats);
+            },
+            minLng() {
+                return Math.min(...this.lngs);
+            },
+            maxLng() {
+                return Math.max(...this.lngs);
+            },
+            zoom() {
+                let spread = Math.max(Math.abs(this.maxLat - this.minLat), Math.abs(this.maxLng - this.minLng));
+                if (spread > 40) return 4;
+                if (spread > 20) return 5;
+                if (spread > 12) return 6;
+                if (spread > 8) return 7;
+                if (spread > 5) return 8;
+                else return 9;
+            },
+            mapCenter() {
+                return {
+                    lat: ((Math.max(...this.lats) + Math.min(...this.lats)) / 2 * 10000) / 10000,
+                    lng: ((Math.max(...this.lngs) + Math.min(...this.lngs)) / 2 * 10000) / 10000
+                    // lng: Math.round(this.weatherLocations.map(l => parseFloat(l.lng))
+                    //     .reduce((acc, c) => acc + c, 0) / this.weatherLocations.length * 10000) / 10000
+                };
+            },
+
+            mapUrl() {
+                return 'https://maps.darksky.net/@radar,' + this.mapCenter.lat + ',' + this.mapCenter.lng + ',' + this.zoom;
+            } 
         },
 
         methods: {
@@ -52,7 +103,7 @@
                     .then(response => {
                         this.weatherLocations = response.data;
                     });
-            }
+            },
         },
 
         mounted() {
