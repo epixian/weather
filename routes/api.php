431a92594986af873f76d7b21d05ec0f6da32281
epixian <epixian@epixian.com>
2019-09-05 17:09:54 -0400

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -17,8 +17,15 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// WEATHER LOCATIONS
 Route::get('/weatherlocations', 'WeatherLocationsApiController@index');
 Route::post('/weatherlocations', 'WeatherLocationsApiController@store');
+Route::get('/weatherlocations/{weatherLocation}', 'WeatherLocationsApiController@show');
 
+// LOCATIONS
 Route::get('/locations', 'LocationsApiController@index');
-Route::post('/locations', 'LocationsApiController@store');
+Route::get('/locations/{location}', 'LocationsApiController@show');
+
+// CUSTOMER->LOCATIONS
+Route::get('/customers/{customer}/locations', 'CustomerLocationsApiController@index');
+Route::post('/customers/{customer}/locations', 'CustomerLocationsApiController@store');
