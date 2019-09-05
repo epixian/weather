adfebc354798b2c1fd1234f0eb2c8a05953d26f5
epixian <epixian@epixian.com>
2019-09-05 17:12:05 -0400

    Update routes.js

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,18 +1,46 @@
 import VueRouter from 'vue-router';
 
 let routes = [
+    // default route
     {
         path: '/',
+        component: () => import('./views/Customers')
+    },
+
+    // customers
+    // {
+    //     path: '/customers',
+    //     component: () => import('./views/Customers')          
+    // },
+    // {
+    //     path: '/customers/:id',
+    //     component: () => import('./views/Customer'),
+    //     children: [
+    //         { path: '', component: () => import('./views/CustomerHome') },
+    //         { path: 'locations', component: () => import('./views/CustomerLocations') }
+    //     ]
+    // },
+
+    // locations
+    {
+        path: '/locations',
         component: () => import('./views/Locations')
     },
     {
+        path: '/locations/:id',
+        component: () => import('./views/Location')
+    },
+
+    // weather locations
+    {
         path: '/weatherlocations',
         component: () => import('./views/WeatherLocations')
-    }
-    // {
-    //     path: '/login',
-    //     component: () => import('./views/Login')
-    // },
+    },
+    {
+        path: '/weatherlocations/:id',
+        component: () => import('./views/WeatherLocation')
+    },
+
 ];
 
 export default new VueRouter({
