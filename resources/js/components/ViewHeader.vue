5737128c7d57b7dfe71670d94338e5b35d75361c
epixian <epixian@epixian.com>
2019-09-05 17:10:31 -0400

    Create ViewHeader.vue

---
+++ resources/js/components/ViewHeader.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="hero" class="hero is-info is-bold">
+        <div class="hero-body">
+            <div class="container">
+                <h1 class="title" v-text="title"></h1>
+                <h2 class="subtitle" v-if="subtitle" v-text="subtitle"></h2>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'view-header',
+
+        props: [ 'title', 'subtitle' ]
+    }
+</script>
