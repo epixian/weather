48f528d3974dcf9416cfcdcf214f1e4ebd1f35aa
epixian <epixian@epixian.com>
2019-09-05 17:10:17 -0400

    Create Customers.vue

---
+++ resources/js/views/Customers.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="view-customers">
+        <view-header
+            :title="title"
+            :subtitle="subtitle"></view-header>
+
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    import ViewHeader from '../components/ViewHeader.vue';
+
+    export default {
+        components: { ViewHeader },
+
+        data() {
+            return {
+                title: 'Customers',
+                subtitle: 'Leads, clients, vendors, and any other organizations you do business with'
+            }
+        }
+    }
+</script>
