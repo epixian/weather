28ed37e5fe49b25240ab7d34329fbaaa21b03104
epixian <epixian@epixian.com>
2019-09-05 17:00:39 -0400

    add scheduled jobs to get weather every 30 minutes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\WeatherLocation;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -26,6 +27,11 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+        $schedule->call(function () {
+            WeatherLocation::all()->each(function ($location) {
+                $location->getCurrentForecast();
+            });
+        })->everyThirtyMinutes();
     }
 
     /**
